d5031d9d4dcc5a56963388f138e508a3ca3732aa
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-15 11:47:01 -0600

    added header and css stylesheet

--- index.php
+++ index.php
@@ -5,10 +5,14 @@
 		<title>
 			Netflix TV Show Data Design Project
 		</title>
+		<link href="style.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-		<p>
-			Test
-		</p>
+		<header>
+			<h1>Netflix TV Show Data Design Project</h1>
+		</header>
+		<main>
+
+		</main>
 	</body>
 </html>
