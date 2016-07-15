ec1f94cd133038d0f2a188ec3b8a1b62ee2644fe
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-15 11:49:28 -0600

    styled header in css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<title>
 			Netflix TV Show Data Design Project
 		</title>
-		<link href="style.css" rel="stylesheet" type="text/css" />
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
 		<header>
