42b24e458c6206bedf6a04bcac289e9dda86b4ca
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-18 08:09:23 -0600

    added persona and some use cases

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="eng">
+<html lang="en">
 	<head>
 		<meta charset="utf-8" />
 		<title>
@@ -9,10 +9,32 @@
 	</head>
 	<body>
 		<header>
-			<h1>Netflix TV Show Data Design Project</h1>
+			<h1>Netflix TV Show Data Design Project
+			</h1>
 		</header>
 		<main>
-
+			<h2>The following information contains a persona, use case, interaction flow table, and a conceptual model about the landing page of a Netflix TV series.
+			</h2>
+			<h3>
+				Persona:
+			</h3>
+			<ol class="use-case">
+				<li>Age: 20 years old</li>
+				<li>Occupation: Professional Netflix watcher and TV show aficionado</li>
+				<li>Goals: Earn a living by watching every episode of his favorite TV show a world record setting number of times</li>
+				<li>Dislikes: Slow loading content, slow wifi, Comcast, bad aesthetics on websites and apps, an app that is not easy to use</li>
+				<li>Needs from an app: must be easy to use, can't be slow to load features, has too look "sharp", must be set up in a logical order, has to be easy to find intended content</li>
+			</ol>
+			<h3>
+				Use Case:
+			</h3>
+			<ul class="use-case">
+				<li>As a registered user I want to watch a show</li>
+				<li>As a registered user I want to view what episodes of a show are available</li>
+				<li>As a registered user I want to chose a certain episode in a season</li>
+				<li></li>
+				<li></li>
+			</ul>
 		</main>
 	</body>
 </html>
