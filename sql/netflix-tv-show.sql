fcda1e0defa3e0d455d71abc1ed6a0128ece8527
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-20 12:12:23 -0600

    created sql tables

--- sql/netflix-tv-show.sql
+++ sql/netflix-tv-show.sql
@@ -6,6 +6,28 @@ CREATE TABLE series (
 );
 CREATE TABLE episode (
 	episodeId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	episodeSeriesId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	episodeResolution
-)
+	episodeSeriesId INT UNSIGNED NOT NULL,
+	episodeTitle VARCHAR(140) NOT NULL,
+	episodeType VARCHAR(127) NOT NULL,
+	episodeResolution SMALLINT UNSIGNED NOT NULL,
+	INDEX(episodeSeriesId),
+	FOREIGN KEY(episodeSeriesId) REFERENCES series(seriesId),
+	PRIMARY KEY(episodeId)
+);
+CREATE TABLE watchlist (
+	watchlistEpisodeId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	watchlistUserId INT UNSIGNED NOT NULL,
+	INDEX(watchlistEpisodeId),
+	INDEX(watchlistUserId),
+	FOREIGN KEY(watchlistEpisodeId) REFERENCES episode(episodeId),
+	FOREIGN KEY(watchlistUserId) REFERENCES user(userId),
+	PRIMARY KEY(watchlistEpisodeId, watchlistUserId)
+);
+CREATE TABLE user (
+	userId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	userEmail VARCHAR(128) NOT NULL,
+	userName VARCHAR(32) NOT NULL,
+	UNIQUE(userEmail),
+	UNIQUE(userName),
+	PRIMARY KEY(userId)
+);
